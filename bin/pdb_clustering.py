34f593d6d3d347fbb9dc6e5b35b96acb9db0d647
Greg Slodkowicz <jergosh@gmail.com>
2016-02-22 17:45:20 +0000

    It's been a while...

--- bin/pdb_clustering.py
+++ bin/pdb_clustering.py
@@ -4,41 +4,21 @@ import glob
 from os import path
 from argparse import ArgumentParser
 import random
-import utils
+# import utils
 from collections import defaultdict
 from pprint import pprint
+from array import array
+from math import sqrt, exp
+import operator
 
 from Bio import SeqIO
 from Bio import AlignIO
 from Bio import SeqUtils
 from Bio import PDB
-
+import pandas
 import numpy as np
-# from sklearn.cluster import AffinityPropagation
-# from sklearn.cluster import KMeans
 
 import networkx as nx
-
-palette = [[99,204,82],
-[189,89,212],
-[211,77,41],
-[59,64,57],
-[202,207,196],
-[213,168,59],
-[199,150,200],
-[85,110,42],
-[83,59,105],
-[189,208,130],
-[106,218,182],
-[196,70,141],
-[104,116,205],
-[185,133,128],
-[193,218,60],
-[90,138,114],
-[102,42,42],
-[107,164,195],
-[207,74,91],
-[174,116,62]]
 
 re_resid = re.compile("(-?[0-9]+)([A-Z]*)")
 
@@ -50,7 +30,7 @@ def parse_coord(coord):
     if ic == '':
         ic = ' '
 
-    return int(n), ic
+    return ' ', int(n), ic
 
 # TODO Put back the slow version and compare results?
 
@@ -90,71 +70,176 @@ def rcn_clustering(neighbour_map, chain, sign_sites):
     labels = [ res_map[r.id] for r in sign_sites ]
     return labels
 
+def centroid(residue):
+    c = array('f', [0, 0, 0])
 
+    k = 0
+    for at in residue:
+        c += at.get_coord()
+        k += 1
+        
+    return c / k 
+
+def dist(at1, at2):
+    return sqrt(reduce(operator.add, [ (c[0]-c[1])**2 for c in zip(at1, at2) ]))
+        
+def clumps(residues, thr=6):
+    WAP = 0.0
+    centroids = [ centroid(r) for r in residues ]
+
+    for i, r1 in enumerate(centroids):
+        for r2 in centroids[i+1:]:
+            d = dist(r1, r2)
+            WAP += exp(- d**2 / (2*thr**2))
+
+    return WAP
+
+# TODO: Is it worth it to make this into a class (or refactor altogether)
 d = lambda: defaultdict(int)
-cl_found = defaultdict(d)
-cl_max = defaultdict(int)
-cl_number = defaultdict(int)
+class ClusterList():
+    def __init__(self):
+        self.cl_found = defaultdict(d)
+        self.cl_max = defaultdict(int)
+        self.cl_number = defaultdict(int)
 
-def collapse_clusters(labels):
-    clusters = defaultdict(int)
-    cluster_sizes = defaultdict(int)
-    for l in labels:
-        if isnan(l):
-            print >>sys.stderr, labels
-            return {}
-        clusters[l] += 1
+    def collapse_clusters(self, labels):
+        clusters = defaultdict(int)
+        cluster_sizes = defaultdict(int)
+        for l in labels:
+            if isnan(l):
+                print >>sys.stderr, labels
+                return {}
+            clusters[l] += 1
 
-    for cl_size in clusters.values():
-        cluster_sizes[cl_size] += 1
+        for cl_size in clusters.values():
+            cluster_sizes[cl_size] += 1
 
-    return cluster_sizes
+        return cluster_sizes
 
-def process_clusters(labels):
-    cluster_sizes = collapse_clusters(labels)
-    cl_max[max(cluster_sizes.keys())] += 1
-    cl_number[max(labels)+1] += 1
+    def process_clusters(self, labels):
+        cluster_sizes = self.collapse_clusters(labels)
+        self.cl_max[max(cluster_sizes.keys())] += 1
+        self.cl_number[max(labels)+1] += 1
 
-    for cl_size in cluster_sizes:
-        cl_found[cl_size][cluster_sizes[cl_size]] += 1
+        for cl_size in cluster_sizes:
+            self.cl_found[cl_size][cluster_sizes[cl_size]] += 1
 
-def get_pvals(cluster_found, niter):
-    for n in sorted(cl_found, reverse=True):
-        for k in sorted(cl_found[n], reverse=True):
-            if n > 1:
-                cl_found[n-1][k] += cl_found[n][k]
+    def get_pvals(self, niter):
+        for n in sorted(self.cl_found, reverse=True):
+            for k in sorted(self.cl_found[n], reverse=True):
+                if n > 1:
+                    self.cl_found[n-1][k] += self.cl_found[n][k]
+                if k > 1:
+                    self.cl_found[n][k-1] += self.cl_found[n][k]
+
+                self.cl_found[n][k] = min(self.cl_found[n][k]/float(niter), 1.0)
+
+        # Biggest cluster size
+        for k in sorted(self.cl_max, reverse=True):
             if k > 1:
-                cl_found[n][k-1] += cl_found[n][k]
+                self.cl_max[k-1] += self.cl_max[k]
 
-            cl_found[n][k] = min(cl_found[n][k]/float(niter), 1.0)
+            self.cl_max[k] = min(self.cl_max[k]/float(niter), 1.0)
 
-    # Biggest cluster size
-    for k in sorted(cl_max, reverse=True):
-        if k > 1:
-            cl_max[k-1] += cl_max[k]
+        # Number of clusters
+        for k in range(1, max(self.cl_number)+1):
+            if k < max(self.cl_number):
+                self.cl_number[k+1] += self.cl_number[k]
 
-        cl_max[k] = min(cl_max[k]/float(niter), 1.0)
+            self.cl_number[k] = min(self.cl_number[k]/float(niter), 1.0)
 
-    # Number of clusters
-    for k in range(1, max(cl_number)+1):
-        if k < max(cl_number):
-            cl_number[k+1] += cl_number[k]
+def find_sequential(chain, res_id):
+    for r in chain:
+        if r.get_id()[1:] == res_id[1:]:
+            return r
 
-        cl_number[k] = min(cl_number[k]/float(niter), 1.0)
+    return None
+
+def process_pdb(df, pdbdir):
+    cath_id = df.cath_id.iloc[0]
+    pdb_id = df.pdb_id.iloc[0]
+    # ens_id = df.ens_id.iloc[0]
+    # chain_id = df.pdb_chain.iloc[0]
+    # print >>sys.stderr, ens_id, pdb_id, chain_id
+    print >>sys.stderr, pdb_id
+    try:
+        # pdb = p.get_structure(pdb_id, path.join(pdbdir, 'pdb'+pdb_id+'.ent'))
+        pdb = p.get_structure(pdb_id, path.join(pdbdir, pdb_id))
+        # pdb_chain = pdb[0][chain_id]
+        pdb_chain = list(pdb[0])[0]
+    except IOError, e:
+        print >>sys.stderr, "PDB file", pdb_id, "missing!"
+        return
+
+    r_coords = []
+    residues = []
+    for i, row in df.iterrows():
+        if row.omega > args.omega_thr:
+            res_id = parse_coord(row.pdb_coord)
+            try:
+                r = pdb_chain[res_id]
+            except KeyError, e:
+                r = find_sequential(pdb_chain, res_id)
+                if r is None:
+                    raise e
+                print >>sys.stderr, r.get_id()
+
+            r_coords.append(r['CA'].get_coord())
+            residues.append(r)
+
+    if len(r_coords) < 2:
+        # print >>sys.stderr, "<= 1 residue with omega > {}. Exiting".format(args.omega_thr)
+        return
+
+    WAP = clumps(residues)
+    #neighbour_map = compute_neighbours(pdb_chain, args.dist_thr)
+    #labels = rcn_clustering(neighbour_map, pdb_chain, residues)
+
+    # print >>sys.stderr, "Running simulations...",
+    sim_WAP = []
+    for i in range(args.niter):
+        sim_residues = random.sample(pdb_chain, len(residues))
+        sim_WAP.append(clumps(sim_residues))
+
+    # print WAP, sum(sim_WAP) / len(sim_WAP)
+    pval = sum([ WAP < w for w in sim_WAP ]) * 1.0 / args.niter
+    print '\t'.join([ str(it) for it in 
+                      [ cath_id, pdb_id, len(pdb_chain), len(residues), pval ] ])
+    # print '\t'.join([ str(it) for it in 
+    #                   [ ens_id, pdb_id, len(pdb_chain), len(residues), pval ] ])
+
+    # clusterlist = ClusterList()
+    # i = 0
+    # while i < args.niter:
+    #     sim_residues = random.sample(pdb_chain, len(residues)) 
+    #     sim_labels = rcn_clustering(neighbour_map, pdb_chain, sim_residues)
+
+    #     if any([ isnan(it) for it in sim_labels ]):
+    #         continue
+
+    #     clusterlist.process_clusters(sim_labels)
+    #     i += 1
+    
+
+    # print >>sys.stderr, "done."
+    # clusterlist.get_pvals(args.niter)
+
+    # clusters = clusterlist.collapse_clusters(labels)
+    # combined_pval = 1.0
+    # for n in sorted(clusters):
+    #     k = clusters[n]
+    #     combined_pval *= clusterlist.cl_found[n][k]
+
+    # print '\t'.join([ str(it) for it in 
+    #                   [ ens_id, pdb_id, len(pdb_chain), len(labels), combined_pval, clusterlist.cl_max[max(clusters.keys())], clusterlist.cl_number[max(labels)+1] ] ])
 
 p = PDB.PDBParser(QUIET=True)
 
 argparser = ArgumentParser()
 argparser.add_argument("--pdbmap", metavar="pdb_map", type=str, required=True)
-argparser.add_argument("--ensid", metavar="ens_id", type=str, required=True)
-argparser.add_argument("--pdbfile", metavar="pdb_file", type=str, required=True)
-argparser.add_argument("--alnfile", metavar="aln_file", type=str, required=True)
-argparser.add_argument("--mapfile", metavar="map_file", type=str, required=True)
-argparser.add_argument("--colorfile", metavar="color_file", type=str, required=True)
-
+argparser.add_argument("--pdbdir", metavar="pdb_dir", type=str, required=True)
 argparser.add_argument("--dist_thr", metavar="thr", type=float, default=4.0)
 argparser.add_argument("--omega_thr", metavar="thr", type=float, default=1.0)
-
 
 argparser.add_argument("--niter", metavar="n_iter", type=int, required=False, default=0)
 argparser.add_argument("--preference", metavar="preference", type=int, required=False, default=-50)
@@ -162,118 +247,15 @@ argparser.add_argument("--preference", metavar="preference", type=int, required=
 
 args = argparser.parse_args()
 
-for l in open(args.pdbmap):
-    f = l.rstrip().split()
-    if f[0] == args.ensid:
-        chain_id = f[2]
-        pdb_begin_id, pdb_begin_ins = parse_coord(f[6])
-        pdb_end_id, pdb_end_ins = parse_coord(f[7])
-        break
-else:
-    raise ValueError("Ensembl ID " + args.ensid + " not found")
+pdb_map = pandas.read_table(args.pdbmap, dtype={ "cath_id": str, "pdb_id": str, "pdb_coord": str, "omega": np.float64 })
 
-omega_map = []
-for l in open(args.mapfile):
-    f = l.rstrip().split()
-    omega_map.append(f)
+# pdb_map = pandas.read_table(args.pdbmap, 
+#                             names=[ "ens_id", "ens_coord", "uniprot_id", "uniprot_coord", 
+#                                       "pdb_id", "pdb_chain", "pdb_coord", "SS", "rsa", "omega" ],
+#                             dtype={ "ens_id": str, "ens_coord": int, "uniprot_id": str, "uniprot_coord": int,
+#                                     "pdb_id": str, "pdb_chain": str, "pdb_coord": str, "SS": str,
+#                                     "rsa": np.float64, "omega": np.float64 })
 
-seqs = list(AlignIO.read(args.alnfile, 'fasta'))
-
-pdb_seq = seqs[0]
-ens_seq = seqs[1]
-
-pdb_id = pdb_seq.name
-ens_id = ens_seq.name
-
-pdb = p.get_structure(pdb_id, args.pdbfile)
-chain = list(pdb[0][chain_id])
-found_begin_id, found_begin_i, found_begin_id, found_end_i = \
-utils.parse_chain(chain, pdb_begin_id, pdb_begin_ins, pdb_end_id, pdb_end_ins)
-
-chain = chain[found_begin_i:found_end_i+1]
-chain_seq = ''.join([ SeqUtils.seq1(r.resname) for r in chain ])
-# chain_seq = ''.join([ SeqUtils.seq1(r.resname) for r in chain if SeqUtils.seq1(r.resname) != 'X' ])
-assert chain_seq == str(pdb_seq.seq.ungap('-'))
-
-# This is a roundabout way of making sure we look at the same set of proteins
-# regardless of the omega threshold
-r_coords = []
-residues = []
-# Omega   lower   upper   LRT_Stat        Pval    Adj.Pval
-for i, r in enumerate(chain):
-    if float(omega_map[i][0]) > 1.0:
-        r_coords.append(r['CA'].get_coord())
-        # residues.append(''.join((str(_id) for _id in r.id)))
-        residues.append(r)
-
-if len(r_coords) < 2:
-    print >>sys.stderr, "<= 1 residue with omega > {}. Exiting".format(1.0)
-    sys.exit(0)
-
-r_coords = []
-residues = []
-for i, r in enumerate(chain):
-    if float(omega_map[i][0]) > args.omega_thr:
-        r_coords.append(r['CA'].get_coord())
-        # residues.append(''.join((str(_id) for _id in r.id)))
-        residues.append(r)
-
-if len(r_coords) < 2:
-    print >>sys.stderr, "<= 1 residue with omega > {}. Exiting".format(args.omega_thr)
-    sys.exit(0)
-
-
-# r_coords = np.array(r_coords)
-# af = AffinityPropagation(preference=args.preference, max_iter=500).fit(r_coords)
-# af = KMeans(n_clusters=5).fit(r_coords)
-# labels = af.labels_
-
-neighbour_map = compute_neighbours(chain, args.dist_thr)
-labels = rcn_clustering(neighbour_map, chain, residues)
-
-# palette = brewer2mpl.get_map('Spectral', 'Diverging', max(labels)+1)
-# if max(labels) < 20:
-#     colorfile = open(args.colorfile, 'w')
-
-#     for i, r in enumerate(residues):
-#         l = labels[i]
-#         print >>colorfile, '\t'.join(str(it) for it in [r] + palette[l])
-
-print >>sys.stderr, "Running simulations...",
-i = 0
-while i < args.niter:
-    sim_residues = random.sample(chain, len(residues)) 
-    sim_labels = rcn_clustering(neighbour_map, chain, sim_residues)
-
-    # af = AffinityPropagation(preference=args.preference, max_iter=500).fit(sim_coords)
-    # sim_labels = af.labels_
-
-    if any([ isnan(it) for it in sim_labels ]):
-        # print >>sys.stderr, "NaNs in SeqUtils.seq1(r2.resname) sim_labels"
-        continue
-
-    process_clusters(sim_labels)
-    # af = KMeans(n_clusters=5).fit(sim_coords)
-    i += 1
-    
-    # colorfile = open(args.colorfile+'_'+str(i), 'w')
-
-    #for r_i, r in enumerate(sim_residues):
-    #    l = sim_labels[r_i]
-    #    print >>colorfile, '\t'.join(str(it) for it in [''.join( [ str(it) for it in r.id] ) ] + palette[l])
-
-print >>sys.stderr, "done."
-get_pvals(cl_found, args.niter)
-
-# for d in cl_found:
-#     print d
-#     print "\t", cl_found[d]
-
-clusters = collapse_clusters(labels)
-combined_pval = 1.0
-for n in sorted(clusters):
-    k = clusters[n]
-    combined_pval *= cl_found[n][k]
-    # print '{}\t{}\t{}\t{}'.format(args.ensid, n, k, cl_found[n][k])
-print '\t'.join([ str(it) for it in 
-                   [ ens_id, pdb_id, len(chain), len(labels), combined_pval, cl_max[max(clusters.keys())], cl_number[max(labels)+1] ] ])
+pdb_map.groupby(["cath_id"]).apply(process_pdb, args.pdbdir)
+# pdb_map.groupby(["ens_id", "pdb_id", "pdb_chain"]).apply(process_pdb, args.pdbdir)
+# process_pdb(pdb_map)
