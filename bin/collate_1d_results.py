f0bbaa9dbb9c7e8cc48d8abe0de5bd57bf8d953d
Greg Slodkowicz <jergosh@gmail.com>
2016-09-06 14:52:36 +0100

    Added a script for collating the 1D clustering results.

---
+++ bin/collate_1d_results.py
@@ -0,0 +1,27 @@
+import os
+from os import path
+import sys
+import argparse
+import glob
+
+def main():
+    argparser = argparse.ArgumentParser()
+
+    argparser.add_argument('--indir', metavar='slr_file', type=str, required=True)
+    argparser.add_argument('--outfile', metavar='out_file', type=str, required=True)
+
+    args = argparser.parse_args()
+
+    outfile = open(args.outfile, 'w')
+    
+    for f in glob.glob(path.join(indir, '*', '*.out')):
+        basename = path.basename(f)
+
+        ens_id = basename.split('_')[0]
+        dataset = '_'.join(basename.split('_')[1:3])
+        print ens_id
+
+        for l in open(f):
+            ## TERRIBLE
+            f = eval(l)
+            print >>outfile, '\t'.join([ ens_id, dataset ] + f)
