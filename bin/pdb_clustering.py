e91b8fbe6d0b9a45d51defb18ecef7580b317402
Greg Slodkowicz <jergosh@gmail.com>
2016-09-12 18:49:13 +0100

    Fixed a typo.

--- bin/pdb_clustering.py
+++ bin/pdb_clustering.py
@@ -61,7 +61,7 @@ def run_graph(sel_residues, all_residues, thr, niter, rerun_thr, rerun_iter):
     neighbour_map = compute_neighbours(all_residues, dist_thr=6)
 
     max_clust, max_clust_id, n_clusts, res_map = graph_clustering(neighbour_map, sel_residues)
-    max_labels = [ r.id for r, i, in res_map.items() if i == max_clust ]
+    max_labels = [ r.id for r, i, in res_map.items() if i == max_clust_id ]
     print max_labels
     # labels = [ res_map[r.id] for r in sel_residues ]
 
